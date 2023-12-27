6b9828799618a74b6bedb2fc9b5583525192ce4d
papiasen <150432274+papiasen@users.noreply.github.com>
2023-12-27 12:45:04 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const appSettings = {
+    databaseURL: "https://playing-around-17c5e-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const notesAppinDB = ref(database, "notesList")
+
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const notesList = document.getElementById("notes-list")
+
+addButtonEl.addEventListener("click", function () {
+    let inputValue = inputFieldEl.value
+    push(notesAppinDB, inputValue)
+    clearInputField()
+})
+
+
+onValue(notesAppinDB, function (snapshot) {
+
+
+    if (snapshot.exists()) {
+
+        let itemsArray = Object.entries(snapshot.val())
+        console.log(itemsArray)
+
+        clearNotesList()
+
+        for (let i = 0; i < itemsArray.length; i++) {
+
+            let currentItem = itemsArray[i]
+
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+
+            notesListValues(currentItem)
+        }
+    } else {
+        notesList.innerHTML = `<p> No items here... yet </p>`
+    }
+
+
+
+})
+
+
+function clearNotesList() {
+    notesList.innerHTML = ""
+}
+
+
+function clearInputField() {
+    inputFieldEl.value = ""
+}
+
+
+function notesListValues(item) {
+    // groceryList.innerHTML += `<li>${itemValue}</li>`
+
+    // console.log(item)
+
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+
+    newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function () {
+        let exactLocationOfItemInDB = ref(database, `notesList/${itemID}`)
+        remove(exactLocationOfItemInDB)
+    })
+
+    notesList.append(newEl)
+}
